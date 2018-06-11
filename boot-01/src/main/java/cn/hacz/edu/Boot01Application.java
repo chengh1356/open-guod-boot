33a8e29ec750200be518edc891aa7aaa90543f49
guod <s121528@163.com>
2018-06-11 19:23:38 +0800

    boot-hello

--- boot-01/src/main/java/cn/hacz/edu/Boot01Application.java
+++ boot-01/src/main/java/cn/hacz/edu/Boot01Application.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 
 /**
- * project -
+ * project - spring boot
  *
  * @author guod
  * @version 1.0
  * @date 日期:2018/6/8 时间:18:13
  * @JDK 1.8
- * @Description 功能模块：
+ * @Description 功能模块：spring boot的hello world入门
  */
 @SpringBootApplication
 @Controller
@@ -19,5 +19,4 @@ public class Boot01Application {
     public static void main(String[] args) {
         SpringApplication.run(Boot01Application.class, args);
     }
-
 }
