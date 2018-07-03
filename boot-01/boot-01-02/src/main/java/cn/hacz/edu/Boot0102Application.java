251c3b7dd09a549e1aa87a2cfee7ef317adc568c
guodd <guodd369@163.com>
2018-07-03 22:08:21 +0800

    boot基础入门配置

---
+++ boot-01/boot-01-02/src/main/java/cn/hacz/edu/Boot0102Application.java
@@ -0,0 +1,20 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project - ETC发票系统
+ *
+ * @author dong
+ * @version 3.0
+ * @date 日期:2018/7/3 时间:21:53
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+@SpringBootApplication
+public class Boot0102Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot0102Application.class, args);
+    }
+}
