6286ff5889de5c7c0b9729f903aa4e0434e3feef
guod <s121528@163.com>
2018-07-13 14:31:14 +0800

    boot企业级开发-

---
+++ boot-04/boot-04-14/src/main/java/cn/hacz/edu/ios/MainFileUtils.java
@@ -0,0 +1,16 @@
+package cn.hacz.edu.ios;
+
+/**
+ * project -
+ *
+ * @author guod
+ * @version 1.0
+ * @date 日期:2018/7/13 时间:8:58
+ * @JDK 1.8
+ * @Description 功能模块：文件操作工具类：FileUtils
+ */
+public class MainFileUtils {
+    public static void main(String[] args) {
+
+    }
+}
