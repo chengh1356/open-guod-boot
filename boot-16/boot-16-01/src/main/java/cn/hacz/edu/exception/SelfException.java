675460375efdf48b94792aadff18fc99d6879413
guod <s121528@163.com>
2018-07-26 19:59:04 +0800

    boot企业级开发-数据加密操作

--- boot-16/boot-16-01/src/main/java/cn/hacz/edu/exception/SelfException.java
+++ boot-16/boot-16-01/src/main/java/cn/hacz/edu/exception/SelfException.java
@@ -5,11 +5,11 @@ import cn.hacz.edu.enums.ResultEnum;
 /**
  * project - ETC发票系统
  *
- * @author dong
+ * @author guod
  * @version 3.0
  * @date 日期:2018/7/9 时间:20:41
  * @JDK 1.8
- * @Description 功能模块：
+ * @Description 功能模块：指定自己的错误吗
  */
 public class SelfException extends RuntimeException {
     private String code;
