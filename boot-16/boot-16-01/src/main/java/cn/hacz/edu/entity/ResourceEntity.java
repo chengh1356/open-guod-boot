0e8abf41e4755b64aa73f412b3a4724b27d4dc05
guodd <guodd369@163.com>
2018-08-15 21:40:22 +0800

    角色管理

---
+++ boot-16/boot-16-01/src/main/java/cn/hacz/edu/entity/ResourceEntity.java
@@ -0,0 +1,30 @@
+package cn.hacz.edu.entity;
+
+import cn.hacz.edu.entity.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * project -
+ *
+ * @author dong
+ * @version 1.0
+ * @date 日期:2018/8/15 时间:21:27
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "1601_resource")
+@DynamicInsert(true)
+@DynamicUpdate(true)
+public class ResourceEntity extends BaseEntity {
+}
