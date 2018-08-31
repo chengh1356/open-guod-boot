29f092e0e5001c73ec08cc3e02a565af50ef51f2
guodd <guodd369@163.com>
2018-08-31 23:09:35 +0800

    项目练习

--- boot-16/boot-16-01/src/main/java/cn/hacz/edu/Boot1601Application.java
+++ boot-16/boot-16-01/src/main/java/cn/hacz/edu/Boot1601Application.java
@@ -1,21 +1,10 @@
 package cn.hacz.edu;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * project - ETC发票系统
@@ -33,21 +22,5 @@ import java.time.format.DateTimeFormatter;
 public class Boot1601Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Boot1601Application.class).run(args);
-    }
-
-    /**
-     * 功能描述：ResponseBody格式化LocalDateTime的全局配置；
-     *
-     * @return
-     */
-    @Bean(name = "mapperObject")
-    public ObjectMapper getObjectMapper() {
-        ObjectMapper om = new ObjectMapper();
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-        om.registerModule(javaTimeModule);
-        return om;
     }
 }
--- boot-16/boot-16-01/src/main/java/cn/hacz/edu/config/JacksonConfig.java
+++ boot-16/boot-16-01/src/main/java/cn/hacz/edu/config/JacksonConfig.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +15,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * project -
@@ -27,14 +35,28 @@ public class JacksonConfig {
     @Primary
     @ConditionalOnMissingBean(ObjectMapper.class)
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+        // ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * 功能描述：ResponseBody格式化LocalDateTime的全局配置；
+         *
+         * @return
+         */
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        objectMapper.registerModule(javaTimeModule);
+        /**
+         * 功能描述：通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+         *
+         * @return
+         */
         // Include.Include.ALWAYS 默认
         // Include.NON_DEFAULT 属性为默认值不序列化
         // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
         // Include.NON_NULL 属性为NULL 不序列化,就是为null的字段不参加序列化
         //objectMapper.setSerializationInclusion(Include.NON_EMPTY);
-
         // 字段保留，将null值转为""
         objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
             @Override
