76e06951f3cb6e6569ee7b52a4a01f0ebb8f400a
guod <s121528@163.com>
2018-09-28 19:50:36 +0800

    Excel导入导出

--- boot-05/boot-05-05/src/main/java/cn/hacz/edu/Boot0505Application.java
+++ boot-05/boot-05-05/src/main/java/cn/hacz/edu/Boot0505Application.java
@@ -1,7 +1,17 @@
 package cn.hacz.edu;
 
+import cn.hacz.edu.util.FileUtil;
+import cn.hacz.edu.vo.Person;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * project - 综合客户服务系统
@@ -13,8 +23,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description 功能模块：
  */
 @SpringBootApplication
+@RestController
 public class Boot0505Application {
     public static void main(String[] args) {
         SpringApplication.run(Boot0505Application.class, args);
     }
+
+    @RequestMapping("export")
+    public void export(HttpServletResponse response) {
+        //模拟从数据库获取需要导出的数据
+        List<Person> personList = new ArrayList<>();
+        Person person1 = new Person("路飞", "1", new Date());
+        Person person2 = new Person("娜美", "2", new Date());
+        Person person3 = new Person("索隆", "1", new Date());
+        Person person4 = new Person("小狸猫", "1", new Date());
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        //导出操作
+        FileUtil.exportExcel(personList, "花名册", "草帽一伙", Person.class, "海贼王.xls", response);
+    }
+
 }
