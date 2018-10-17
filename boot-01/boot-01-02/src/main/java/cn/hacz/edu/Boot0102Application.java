8c9a369412d96b516adb30145c487db1c7f900ee
guod <s121528@163.com>
2018-10-17 18:44:22 +0800

    生成二维码

--- boot-01/boot-01-02/src/main/java/cn/hacz/edu/Boot0102Application.java
+++ boot-01/boot-01-02/src/main/java/cn/hacz/edu/Boot0102Application.java
@@ -1,7 +1,11 @@
 package cn.hacz.edu;
 
+import cn.hacz.edu.properties.PayProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * project - ETC发票系统
@@ -13,8 +17,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @Description 功能模块：
  */
 @SpringBootApplication
+@RestController
 public class Boot0102Application {
+    @Autowired
+    private PayProperties payProperties;
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(Boot0102Application.class).run(args);
+    }
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return payProperties.getPayName();
     }
 }
---
+++ boot-01/boot-01-02/src/main/java/cn/hacz/edu/util/MainTest.java
@@ -0,0 +1,27 @@
+package cn.hacz.edu.util;
+
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * project - 综合客户服务系统
+ *
+ * @author guod
+ * @version 1.0
+ * @date 日期:2018/10/17 时间:14:15
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+public class MainTest {
+    public static void main(String[] args) throws IOException {
+        File file = ResourceUtils.getFile("application.properties");
+        Properties properties = PropertiesLoaderUtils.loadAllProperties("classpath:conf/pay.properties");
+        String o = (String) properties.get("pay.name");
+        String property = properties.getProperty("server.port");
+        System.out.println(o);
+    }
+}
