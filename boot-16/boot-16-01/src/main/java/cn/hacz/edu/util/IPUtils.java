0c962dfc11b2d99900a64acb32b8591789d9ac8a
guodd369 <guodd369@163.com>
2018-11-26 13:16:37 +0800

    IP地址

--- boot-16/boot-16-01/src/main/java/cn/hacz/edu/util/IPUtils.java
+++ boot-16/boot-16-01/src/main/java/cn/hacz/edu/util/IPUtils.java
@@ -51,6 +51,7 @@ public class IPUtils {
         // 使用代理，则获取第一个IP地址
         if (StringUtils.isEmpty(ip) && ip.length() > 15) {
             if (ip.indexOf(",") > 0) {
+
                 ip = ip.substring(0, ip.indexOf(","));
             }
         }
