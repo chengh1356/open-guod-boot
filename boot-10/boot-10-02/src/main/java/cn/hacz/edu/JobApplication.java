e7b7792a28ecc964bacc51d5028f2549fe17d058
guodd369 <guodd369@163.com>
2018-12-14 16:03:59 +0800

    EJBQL

---
+++ boot-10/boot-10-02/src/main/java/cn/hacz/edu/JobApplication.java
@@ -0,0 +1,18 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project -
+ *
+ * @author guodd
+ * @version V1.0
+ * @date 日期:2018/12/13 时间:18:30
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+@SpringBootApplication
+public class JobApplication {
+    public static void main(String[] args) {
+    }
+}
