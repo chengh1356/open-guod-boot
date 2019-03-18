2525a549a45302a25883a36017f1da0ecd3703c3
guodd369 <guodd369@163.com>
2019-03-18 18:26:00 +0800

    kafka

--- boot-03/boot-03-02/src/main/java/cn/hacz/edu/Boot0302Application.java
+++ boot-03/boot-03-02/src/main/java/cn/hacz/edu/Boot0302Application.java
@@ -1,7 +1,10 @@
 package cn.hacz.edu;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * project - GitHub整理
@@ -13,8 +16,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description 功能模块：
  */
 @SpringBootApplication
+@Slf4j
+@RestController
 public class Boot0302Application {
     public static void main(String[] args) {
         SpringApplication.run(Boot0302Application.class, args);
+    }
+
+    @GetMapping("/index")
+    public String a() {
+        log.trace("======trace info");
+        log.debug("======debug");
+        log.info("======info");
+        log.warn("======warn");
+        log.error("======error");
+        return "123";
     }
 }
